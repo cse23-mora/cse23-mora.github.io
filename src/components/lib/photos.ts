856717677defec59ba13c713d0e77c54371bd528
PANKAJA D.L.K <pankajadlk.23@uom.lk>
2025-05-23 09:51:42 +0530

    update gallert

--- src/components/lib/photos.ts
+++ src/components/lib/photos.ts
@@ -6,39 +6,57 @@ const items = [
       className: "absolute top-10 left-[20%] rotate-[-5deg]",
     },
     {
-      title: "The Narrator",
+      title: "්‍රිප් පික්කෝ",
       image:
-        "https://images.unsplash.com/photo-1697909623564-3dae17f6c20b?q=80&w=2667&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "/images/gallery/pic1.jpg",
       className: "absolute top-40 left-[25%] rotate-[-7deg]",
     },
     {
-      title: "Iceland",
+      title: "Sakura Deco",
       image:
-        "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "/images/gallery/pic2.jpg",
       className: "absolute top-5 left-[40%] rotate-[8deg]",
     },
     {
-      title: "Japan",
+      title: "Fun time",
       image:
-        "https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "/images/gallery/pic3.jpg",
       className: "absolute top-32 left-[55%] rotate-[10deg]",
     },
     {
-      title: "Norway",
+      title: "",
       image:
-        "https://images.unsplash.com/photo-1421789665209-c9b2a435e3dc?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "images/gallery/pic4.jpg",
       className: "absolute top-20 right-[35%] rotate-[2deg]",
     },
     {
-      title: "New Zealand",
+      title: "",
       image:
-        "https://images.unsplash.com/photo-1505142468610-359e7d316be0?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "images/gallery/pic5.jpg",
       className: "absolute top-24 left-[45%] rotate-[-7deg]",
     },
     {
-      title: "Canada",
+      title: "",
       image:
-        "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "images/gallery/pic6.jpg",
+      className: "absolute top-8 left-[30%] rotate-[4deg]",
+    },
+    {
+      title: "",
+      image:
+        "images/gallery/pic7.jpg",
+      className: "absolute top-8 left-[30%] rotate-[4deg]",
+    },
+    {
+      title: "",
+      image:
+        "images/gallery/pic8.jpg",
+      className: "absolute top-8 left-[30%] rotate-[4deg]",
+    },
+    {
+      title: "",
+      image:
+        "images/gallery/pic9.jpg",
       className: "absolute top-8 left-[30%] rotate-[4deg]",
     },
   ];
