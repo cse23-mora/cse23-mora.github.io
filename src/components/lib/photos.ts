cf22f833cc98aa9a47dc87ea6128df8fa1417a5d
Kalana-Pankaja <pankajadlk.23@uom.lk>
2025-12-07 23:19:28 +0530

    feat: update gallery

--- src/components/lib/photos.ts
+++ src/components/lib/photos.ts
@@ -117,6 +117,108 @@ const items = [
         "/images/gallery/pic19.webp",
       className: "absolute top-8 left-[30%] rotate-[4deg]",
     },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic8.webp",
+      className: "absolute top-16 left-[35%] rotate-[-6deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic20.webp",
+      className: "absolute top-12 left-[48%] rotate-[5deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic21.webp",
+      className: "absolute top-28 left-[22%] rotate-[-8deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic22.webp",
+      className: "absolute top-6 left-[52%] rotate-[3deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic23.webp",
+      className: "absolute top-36 left-[42%] rotate-[-4deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic24.webp",
+      className: "absolute top-14 left-[28%] rotate-[7deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic25.webp",
+      className: "absolute top-22 left-[38%] rotate-[-5deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic26.webp",
+      className: "absolute top-10 left-[50%] rotate-[6deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic27.webp",
+      className: "absolute top-30 left-[32%] rotate-[-3deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic28.webp",
+      className: "absolute top-18 left-[44%] rotate-[8deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic29.webp",
+      className: "absolute top-26 left-[26%] rotate-[-7deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic30.webp",
+      className: "absolute top-12 left-[36%] rotate-[4deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic31.webp",
+      className: "absolute top-34 left-[46%] rotate-[-6deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic32.webp",
+      className: "absolute top-8 left-[24%] rotate-[5deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic33.webp",
+      className: "absolute top-20 left-[54%] rotate-[-4deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic34.webp",
+      className: "absolute top-24 left-[34%] rotate-[6deg]",
+    },
+    {
+      title: "",
+      image:
+        "/images/gallery/pic35.webp",
+      className: "absolute top-16 left-[40%] rotate-[-5deg]",
+    },
 
   ];
 
